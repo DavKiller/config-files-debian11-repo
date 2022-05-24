560548e7ea4274f2c0d8c908bc307162e0ad0380
DavKiller <davidflor46@gmail.com>
2022-05-24 11:06:25 +0200

    Update downloader.sh

--- downloader.sh
+++ downloader.sh
@@ -14,7 +14,7 @@ BLUE='\033[01;32m'
 YELLOW='\033[01;33m'
 
 # Comprobamos que se ha introducido como minimo un parametro.
-if [ -z $1 ]; then 
+if [ -z "$1" ]; then 
 	echo -e "${RED}Debe pasar como minimo un parametro para continuar. ${RESTORE}"
 	exit 1
 fi
@@ -27,15 +27,15 @@ do
 BUSQUEDA=$(apt search $PACK 2>/dev/null | cut -d/ -f1  | grep -x "$PACK")
 
 # Si el parametro no existe en el repositorio de Debian, se muestra un aviso y se sale del script.
-if [ -z $BUSQUEDA ]; then
+if [ -z "$BUSQUEDA" ]; then
 	echo -e "${YELLOW}El paquete $PACK no existe o el nombre del paquete es incorrecto.${RESTORE}"
 	exit 1
 
 # Si el parametro es correcto, comienza a descargar el paquete.
-elif [ $BUSQUEDA != "" ]; then
+elif [ "$BUSQUEDA" != "" ]; then
 	echo -e "${GREEN}El paquete $BUSQUEDA ha sido encontrado para su descarga.${RESTORE}"
-	cd $R_PACK
-	apt-get download $BUSQUEDA
+	cd "$R_PACK"
+	apt-get download "$BUSQUEDA"
 	apt-get download -q $(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends $BUSQUEDA | grep "^\w" )	
 echo -e "El paquete ${BLUE}$BUSQUEDA${RESTORE} ha sido descargado." 
 fi 
@@ -43,7 +43,7 @@ done
 
 # Una vez terminado el bucle se añaden al repositorio local los paquetes descargados.
 echo -e "Añadiendo al repositorio el/los paquetes..."
-	cd $R_REPO 
+	cd "$R_REPO" 
 	# Testing es la rama que he usado para la creacion del repositorio de prueba.
 	# Si se ha creado con el codename bullseye se debera cambiar el nombre de testing a bullseye en la variable del script para su funcionamiento.
 	reprepro -s includedeb $CODENAME $R_PACK/*.deb 2>skip-error.txt
